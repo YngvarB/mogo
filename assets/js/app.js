db03eb901a2319b6c3c946330e1044a3f13442ee
YngvarB <81563139+YngvarB@users.noreply.github.com>
2021-05-17 05:21:20 +0300

    v1.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -47,6 +47,33 @@ $('[data-scroll]').on('click', function(event) {
     $('#nav_toggle').removeClass('active');
 });
 
+ /* Smooth scroll */
+//  $("[data-scroll]").on("click", function(event) {
+//     event.preventDefault();
+  
+//     var $this = $(this),
+//      blockId = $this.data('scroll'),
+//      blockOffset = $(blockId).offset().top - $('header').innerHeight();
+  
+//     $("#nav a").removeClass('active')
+//     $this.addClass('active');
+  
+//     if ($("#nav_toggle").hasClass('active')) {
+//      $(window).on("scroll", function() {
+//       $('#nav_toggle').removeClass("active");
+//       $("#nav").removeClass("active");
+//      });
+//     }
+  
+//     $('.section__title').removeClass('bg-highlight')
+//     $(blockId + ' .section__title').addClass('bg-highlight')
+  
+//     $("html, body").animate({
+//      scrollTop: blockOffset
+//     }, 500);
+//    });
+  
+
 
 /* Menu nav toggle */
 
